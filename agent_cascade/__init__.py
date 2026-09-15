d0b9467e939008cf48494731762f23723dd497a1
LuckyKo <gunoimail@gmail.com>
2026-09-16 00:18:44 +0300

    chore(version): bump to 0.1.45

--- agent_cascade/__init__.py
+++ agent_cascade/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '0.1.44'
+__version__ = '0.1.45'
 
 
 # Lazy imports to avoid circular dependency deadlocks when importing submodules
